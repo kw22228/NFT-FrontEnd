f95e2c17312f26c2f6bb3863fcd7d524d2c948d2
kw22228 <hot5521@naver.com>
2022-07-12 18:11:30 +0900

    init: global style

---
+++ frontend/src/styles/variouble.ts
@@ -0,0 +1,13 @@
+const variouble = {
+    fontxs: '0.75em',
+    fontsm: '0.875em',
+    fontmd: '1em', //16px
+    fontlg: '1.25em',
+    fontxl: '2em',
+    fontxxl: '3em',
+    fontxxxl: '4em',
+
+    navHeight: '5rem',
+};
+
+export default variouble;
