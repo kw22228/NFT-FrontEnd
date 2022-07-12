f453b01ff7be1291ff4efa14e286102c8fca8db7
kw22228 <hot5521@naver.com>
2022-07-12 18:10:31 +0900

    init: logo component

---
+++ frontend/src/components/Logo/Logo.style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Logo = styled.div`
+    background-color: gray;
+    padding: 1.5rem;
+
+    border-radius: 50%;
+
+    cursor: pointer;
+`;
---
+++ frontend/src/components/Logo/Logo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as s from './Logo.style';
+
+const Logo = () => {
+    return <s.Logo>Logo</s.Logo>;
+};
+
+export default Logo;
