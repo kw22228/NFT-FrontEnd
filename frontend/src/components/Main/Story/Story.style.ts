8edbe0e0b5cfbebb4e8dc4f24dd09f5af8289b3e
김재원 <kw2282222@gmail.com>
2022-07-13 22:37:16 +0900

    init: stroy component

--- frontend/src/components/Main/Story/Story.style.ts
+++ frontend/src/components/Main/Story/Story.style.ts
@@ -3,11 +3,12 @@ import styled from 'styled-components';
 export const Section = styled.section`
     height: 200vh;
     width: 100vw;
-    background-color: pink;
 
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    position: relative;
 `;
 
 export const Title = styled.h1`
@@ -18,8 +19,52 @@ export const Title = styled.h1`
     width: fit-content;
 `;
 
-export const Container = styled.div``;
+export const Container = styled.div<{ direction?: String }>`
+    display: flex;
+    flex-direction: ${props => (props.direction === 'reverse' ? 'row-reverse' : 'row')};
+    justify-content: center;
+    align-items: center;
 
-export const ImgBox = styled.div``;
+    width: 80%;
+    height: calc(100% / 3);
 
-export const TextBox = styled.div``;
+    margin: 1rem auto;
+
+    & > div:first-child {
+        margin-right: ${props => (props.direction === 'reverse' ? '0' : '2rem')};
+        margin-left: ${props => (props.direction === 'reverse' ? '2rem' : '0')};
+    }
+`;
+
+export const ImgBox = styled.div`
+    width: 45%;
+    height: 70%;
+    background-color: #d9d9d9;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const TextBox = styled.div`
+    width: 45%;
+    height: 40%;
+    background-color: #d9d9d9;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Ball = styled.div`
+    background-color: #f5f5f5;
+
+    padding: 10rem;
+    border-radius: 50%;
+
+    position: absolute;
+    top: 25%;
+    left: 0%;
+
+    z-index: -1;
+`;
--- frontend/src/components/Main/Story/Story.tsx
+++ frontend/src/components/Main/Story/Story.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import * as s from './Story.style';
 
+interface IStoryBoard {
+    img: string;
+    text: string;
+    direction?: string;
+}
+
+const StoryBoard = ({ img, text, direction = 'row' }: IStoryBoard) => {
+    return (
+        <s.Container direction={direction}>
+            <s.ImgBox>{img}</s.ImgBox>
+            <s.TextBox>{text}</s.TextBox>
+        </s.Container>
+    );
+};
+
 const Story = () => {
     return (
         <s.Section>
             <s.Title>STORY</s.Title>
-            <s.Container>
-                <s.ImgBox></s.ImgBox>
-                <s.TextBox></s.TextBox>
-            </s.Container>
+            <StoryBoard img="Image1" text="Text1" />
+            <StoryBoard img="Image2" text="Text3" direction="reverse" />
+            <StoryBoard img="Image3" text="Text3" />
+
+            <s.Ball />
         </s.Section>
     );
 };
