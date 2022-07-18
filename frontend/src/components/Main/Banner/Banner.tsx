067ad7b0ce913a6fa19156af0b1572dd23b2532e
kw22228 <hot5521@naver.com>
2022-07-18 18:05:18 +0900

    update: banner color imges

--- frontend/src/components/Main/Banner/Banner.tsx
+++ frontend/src/components/Main/Banner/Banner.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import * as s from './Banner.style';
 
 import waves from '../../../assets/images/waves.svg';
+import main from '../../../assets/images/main.jpg';
+import character from '../../../assets/images/banner_1.png';
 
 const Banner = () => {
     return (
         <s.Section>
-            Banner Image
+            <s.BannerContainer main={main}>
+                <s.Character src={character} alt={character} />
+            </s.BannerContainer>
             <s.Wave src={waves} alt="waves" />
         </s.Section>
     );
