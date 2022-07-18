b6151b1e079383c24109eb336074f54e714d9b4e
kw22228 <hot5521@naver.com>
2022-07-18 18:04:50 +0900

    update: logo style

--- frontend/src/components/Logo/Logo.style.ts
+++ frontend/src/components/Logo/Logo.style.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Logo = styled.div`
-    background-color: gray;
-    padding: 1.5rem;
-
-    border-radius: 50%;
+    background-color: ${props => props.theme.body};
+    padding: 1.5rem 4rem;
+    border-radius: 15px;
 
     cursor: pointer;
 `;
+
+export const LogoLink = styled(Link)``;
--- frontend/src/components/Logo/Logo.tsx
+++ frontend/src/components/Logo/Logo.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import * as s from './Logo.style';
 
 const Logo = () => {
-    return <s.Logo>Logo</s.Logo>;
+    return (
+        <s.LogoLink to="/">
+            <s.Logo>Logo</s.Logo>
+        </s.LogoLink>
+    );
 };
 
 export default Logo;
