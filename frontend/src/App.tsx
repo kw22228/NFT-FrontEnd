d22ce36a38ca33aa3d80716948a706cb197d5900
kw22228 <hot5521@naver.com>
2022-07-21 17:25:24 +0900

    fixed: locomotive scroll scrolltop twinkle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
-// import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
-// import 'locomotive-scroll/dist/locomotive-scroll.css';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
+import 'locomotive-scroll/dist/locomotive-scroll.css';
 
 import Header from './components/Header/Header';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import GallaryPage from './page/GallaryPage';
 import MainPage from './page/MainPage';
 import MintPage from './page/MintPage';
-// import ScrollTriggerProxy from './utils/animation/gsap/ScrollTriggerProxy';
+import { useRef } from 'react';
+import ScrollTriggerProxy from './lib/animation/gsap/ScrollTriggerProxy';
 
 function App() {
-    // const containRef = useRef(null);
+    const containRef = useRef(null);
 
     return (
         <BrowserRouter>
-            {/* <LocomotiveScrollProvider //
+            <LocomotiveScrollProvider
                 options={{
                     smooth: true,
                     smartphone: {
@@ -27,25 +28,27 @@ function App() {
                 }}
                 watch={
                     [
-                        //
+                        //..all the dependencies you want to watch to update the scroll.
+                        //  Basicaly, you would want to watch page/location changes
+                        //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
                     ]
                 }
                 containerRef={containRef}
             >
                 <ScrollTriggerProxy />
-                <main className="Main" data-scroll-container ref={containRef}> */}
-            <ScrollToTop>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<MainPage />} />
+                <main className="App" data-scroll-container ref={containRef}>
+                    <ScrollToTop>
+                        <Header />
+                        <Routes>
+                            <Route path="/" element={<MainPage />} />
 
-                    <Route path="/gallary" element={<GallaryPage />} />
-                    <Route path="/mint" element={<MintPage />} />
-                </Routes>
-                {/* </main> */}
-                {/* </LocomotiveScrollProvider> */}
-                <Footer />
-            </ScrollToTop>
+                            <Route path="/gallary" element={<GallaryPage />} />
+                            <Route path="/mint" element={<MintPage />} />
+                        </Routes>
+                        <Footer />
+                    </ScrollToTop>
+                </main>
+            </LocomotiveScrollProvider>
         </BrowserRouter>
     );
 }
