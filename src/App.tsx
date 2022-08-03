84399fa721e6f83e45fc0bc76a18ff68481ad9bf
kw22228 <hot5521@naver.com>
2022-08-03 18:04:14 +0900

    add install

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function App() {
                         <Route path="/gallary" element={<GallaryPage />} />
                     </Routes>
                     <Footer />
-                    <FixedBtn />
+                    {/* <FixedBtn /> */}
                 </main>
             </LocomotiveScrollProvider>
         </BrowserRouter>
