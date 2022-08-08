ff91d4b30a76a34a8dba3682aa64342b4b26a044
kw22228 <hot5521@naver.com>
2022-08-08 17:52:29 +0900

    add: resize event hooks

---
+++ src/lib/hooks/useDebounceResize.ts
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { globalWidthAtom } from '../recoil/atoms';
+import debounce from '../utils/debounce';
+
+const useDebounceResize = () => {
+    const [globalWidth, setGlobalWidth] = useRecoilState(globalWidthAtom);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setGlobalWidth({
+                width: window.innerWidth,
+            });
+        };
+        const debounceHandleResize = debounce(handleResize, 100);
+
+        window.addEventListener('resize', debounceHandleResize);
+
+        return () => window.addEventListener('resize', debounceHandleResize);
+    }, []);
+};
+
+export default useDebounceResize;
