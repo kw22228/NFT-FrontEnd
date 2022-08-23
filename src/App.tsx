bc5efb944b1d8414ea542f89b606055a9b65e4a1
김재원 <kw2282222@gmail.com>
2022-08-23 20:42:27 +0900

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { LocomotiveScrollProvider, Scroll } from 'react-locomotive-scroll';
