4015976fa9d92f8237ad6a66ccce52575befb2a5
91savage <ekdma1403@gmail.com>
2022-08-29 18:46:49 +0900

    1

--- src/lib/web3/search.ts
+++ src/lib/web3/search.ts
@@ -7,11 +7,12 @@ import { ABI, CONTRACTADDRESS } from '../../lib/web3/config';
 export const config: { rpcURL: string } = {
     rpcURL: window.klaytn,
 };
-
 export const caver = new Caver(config.rpcURL);
+let IndexForSearch = 0;
 
 function search() {
-    const accunt = window.klaytn.selectedAddress;
-
     const myContract = new caver.klay.Contract(ABI as AbiItem[], CONTRACTADDRESS);
+    myContract.methods.mintingInformation().call.then(function (result: string[]) {
+        IndexForSearch = parseInt(result[1]);
+    });
 }
